e038a909e71cb45d414fc6f5e38e86631dc4c5fd
jafriyie1 <sfjafthemp@gmail.com>
2019-11-30 00:29:10 -0600

    Added more to retrace algo

--- retrace_gamma.py
+++ retrace_gamma.py
@@ -120,24 +120,37 @@ def test_sarsa_lamda(w):
 
 
 
-
-
-def sarsa_gamma():
+def retrace_gamma():
     env = gym.make('MountainCar-v0')
-    def epsilon_greedy_policy(s,done,w,epsilon=.0):
+    def epsilon_greedy_prob(epsilon, w, done,a):
         nA = env.action_space.n
         Q = [np.dot(w, X(s,done,a)) for a in range(nA)]
 
-        if np.random.rand() < epsilon:
-            return np.random.randint(nA)
+        rand = np.random.random()
+        #print(rand)
+        prob = 0 
+        if rand < epsilon:
+            action = vals.argmax()
+            prob = 1 
         else:
-            return np.argmax(Q)
+            action = env.action_space.sample()
+            prob =  (1/3) * epsilon + (1/3)
+            print('sss: {}'.format(prob))
+        return int(action), prob
 
-    gamma = .5
-    alpha = 0.01 
-
-    num_episodes = 100
-    
+    def expectation_action_all_q(a, w, s, done, prob):
+        expected_value = 0 
+        curr_other_prob = (1 - prob) / 2
+        for a in range(env.action_space.n):
+            
+            if a == action_taken:
+                n_prob = prob
+                #print(prob)
+                val = np.dot(w, X(s,done,a))
+                expected_value += n_prob * val
+            else: 
+                print(curr_other_prob)
+                expected_value += curr_other_prob * val
 
     nA = env.action_space.n
 
@@ -153,6 +166,8 @@ def sarsa_gamma():
 
     gen = GenEstimator(gamma)
     phi_list = []
+
+
     for eps in range(num_episodes):
         print('episode #{}'.format(eps))
         traj_list = []
@@ -183,10 +198,7 @@ def sarsa_gamma():
 
         reward_list.append(reward)
 
-
-
         for u in range(1, T):
-            #print('u val: {}'.format(u))
             state, reward, action, done = traj_list[u]
 
             phi_u  = X(state, done, action)
@@ -202,7 +214,6 @@ def sarsa_gamma():
 
                 #env.render()
 
-
                 state, reward, action, done = traj_list[t]
 
                 phi_t = X(state, done, action)
@@ -212,8 +223,110 @@ def sarsa_gamma():
                             for i in range(t, u-1)   ])
 
 
-                #print(a)
-                #print(a-b)
+                delta = delta + gen(u -t, T - t) * ((a -b)) * phi_t
+                
+                #print(delta)
+            w = w - alpha * delta
+            #print(w)
+
+        phi_list = [] 
+        reward_list = []
+        #t = 0 
+        #T = 200 
+    return w
+
+
+
+
+
+
+
+
+def sarsa_gamma():
+    env = gym.make('MountainCar-v0')
+    def epsilon_greedy_policy(s,done,w,epsilon=.0):
+        nA = env.action_space.n
+        Q = [np.dot(w, X(s,done,a)) for a in range(nA)]
+
+        if np.random.rand() < epsilon:
+            return np.random.randint(nA)
+        else:
+            return np.argmax(Q)
+
+    gamma = .5
+    alpha = 0.01 
+
+    num_episodes = 10
+    
+    nA = env.action_space.n
+
+    X = StateActionFeatureVectorWithTile(
+        env.observation_space.low,
+        env.observation_space.high,
+        env.action_space.n,
+        num_tilings=10,
+        tile_width=np.array([.45,.035])
+    )
+
+    w = np.zeros((X.feature_vector_len()))
+
+    gen = GenEstimator(gamma)
+    phi_list = []
+
+    for eps in range(num_episodes):
+        print('episode #{}'.format(eps))
+        traj_list = []
+        reward_list = []
+
+        state, reward, done = env.reset(), 0, False 
+
+        action = epsilon_greedy_policy(state, done, w)
+
+        traj_list.append((state, reward, action, done))
+
+        T = 0
+       
+        while not done:
+
+            next_state, reward, done, _ = env.step(action)
+            action = int(epsilon_greedy_policy(next_state, done ,w))
+            traj_list.append((next_state, reward, action, done))
+            #t+=1 
+            T += 1
+        
+        #state, reward, done = env.reset(), 0, False 
+
+        #action = epsilon_greedy_policy(state, done, w)
+
+        phi_0 = X(state, done, action)
+        phi_list.append(phi_0)
+
+        reward_list.append(reward)
+
+        for u in range(1, T):
+            state, reward, action, done = traj_list[u]
+
+            phi_u  = X(state, done, action)
+            phi_list.append(phi_u)
+            reward_list.append(reward)
+
+            delta = np.zeros((X.feature_vector_len()))
+
+            for t in range(0, u-1):
+                #print(t)
+                #print(u)
+                #print(len(reward_list))
+
+                #env.render()
+
+                state, reward, action, done = traj_list[t]
+
+                phi_t = X(state, done, action)
+                
+                a = phi_t - pow(gamma, u-t) * phi_u
+                b = sum([pow(gamma, i-t) *  reward_list[i]
+                            for i in range(t, u-1)   ])
+
 
                 delta = delta + gen(u -t, T - t) * ((a -b)) * phi_t
                 
